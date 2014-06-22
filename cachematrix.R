00d83cc8899acae9a4b330588bc9a9e21dccf2eb
josephmkahn <joseph.kahn@gmail.com>
2014-06-22 09:59:02 -0700

    initial commit, modifying instructors cacheMean code using solve for inverse rather than mean

--- cachematrix.R
+++ cachematrix.R
@@ -1,15 +1,51 @@
-## Put comments here that give an overall description of what your
-## functions do
+## Coursera R programming,  assignment 2: develop two functions to
+# 1) create a "cached matrix object" via makeCacheMatrix 
+# 2) retreive the object's cached inverse, or else compute and cache if cache not yet present.
 
-## Write a short comment describing this function
-
+## makeCacheMatrix takes input matric x (or attempts to force into matrix)
+# and returns a list with slots for  functions to retreive the original matrix,
+# and to cache and retreive its inverse
 makeCacheMatrix <- function(x = matrix()) {
 
-}
+  M <- NULL
+  set <- function(y) {
+    x <<- y
+    M <<- NULL
+  }
+  get <- function() x
+  setinverse <- function(inverse) M <<- inverse
+  getinverse <- function() M
+  list(set = set, get = get,
+       setinverse = setinverse,
+       getinverse = getinverse)
+} # end function makeCacheMatrix
 
 
-## Write a short comment describing this function
 
+## cachesolve returns the inverse matrix of x,
+# where x is a chachedMatrix created via makeCacheMatrix function
+# If the cached inverse exists for the matrix, it will be returned,
+# otherwise the inverse will be computed and cached into x as a side effect
 cacheSolve <- function(x, ...) {
         ## Return a matrix that is the inverse of 'x'
-}
+  M <- x$getinverse()
+  if(!is.null(M)) {
+    message("getting cached data")
+    return(M)
+  }
+  data <- x$get()
+  M <- solve(data, ...) # solve of a square matrix with no extra arguments yields inverse
+  x$setinverse(M)
+  M
+} # end function cacheSolve
+
+# Example, no run:
+#Xo <- matrix(rnorm(9,0,1),nrow=3,byrow=TRUE) # initial matrix Xo
+#solve(Xo) # check what regular solve (inverse) gives
+#X <- makeCacheMatrix(Xo)
+#cacheSolve(X)
+#cacheSolve(X) # second time around, uses cached results
+
+
+
+
